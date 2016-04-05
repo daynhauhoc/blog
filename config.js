e437259ce06e10b7715fa0eb22f2f6a0191fc185
Khoa Nguyen <hi@khoanguyen.me>
2016-04-05 08:09:16 +0700

    config(filter): Change fiter posts config

--- config.js
+++ config.js
@@ -3,7 +3,12 @@ export const baseUrlHttp = "http://daynhauhoc.com"
 
 export const condition = {
   "$and": [
-    { like_count: { "$gte" : 10 } },
+    {
+      "$or": [
+        { like_count: { "$gte" : 10 } },
+        { reply_count: { "$gte": 5 } },
+      ],
+    },
     { pinned: false },
   ],
 }
