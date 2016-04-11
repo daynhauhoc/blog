09ae156f8a43449c72f192c18072ba2738e62016
Khoa Nguyen <hi@khoanguyen.me>
2016-04-11 14:37:21 +0700

    feat(topbar): Add a loading indicator

--- web_modules/layouts/index.js
+++ web_modules/layouts/index.js
@@ -1,4 +1,5 @@
 export Page from "./Page" // default layout
 export PageError from "./PageError"
-export Homepage from "./Homepage"
+export PageLoading from "./PageLoading"
 export Post from "./Post"
+export Homepage from "./Homepage"
